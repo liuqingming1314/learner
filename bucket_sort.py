3d3846fa1694ed40a69eed3960d62cd949f86298
liuqingming1314 <69711453+liuqingming1314@users.noreply.github.com>
2020-11-06 16:59:04 +0800

    Update bucket_sort.py

--- bucket_sort.py
+++ bucket_sort.py
@@ -1,7 +1,7 @@
 def bucket_sort(li,n=100,max_num=10000):
     buckets=[[] for _ in range(n)]  #创建桶
     for var in li:
-        i=min(var//(number//n),n-1)    #i表示var放到几号桶
+        i=min(var//(max_num//n),n-1)    #i表示var放到几号桶
         buckets[i].append(var)         #把var放桶里
         #下面排桶里的顺序
         for j in range(len(buckets[i])-1,0,-1):
@@ -9,14 +9,14 @@ def bucket_sort(li,n=100,max_num=10000):
                 buckets[i][j],buckets[i][j-1]=buckets[i][j-1],buckets[i][j]
             else:
                 break
-     sorted_li=[]
-     for buc in buckets:
-         sorted_li.extend(buc)
-     return sorted_li
-    
+    sorted_li=[]
+    for buc in buckets:
+        sorted_li.extend(buc)
+    return sorted_li
+
 import random
 
-li=[random.randint(0,10000) dor in range(100000)]
+li=[random.randint(0,10) for i in range(100)]
 print(li)
 li=bucket_sort(li)
 print(li)
